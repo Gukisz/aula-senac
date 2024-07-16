9c112944d25211c1139121ba8e715d63b76fa5f2
gks <126225075+Gukisz@users.noreply.github.com>
2024-07-16 20:14:42 -0300

    Delete Aula10-04 directory

--- Aula10-04/exercicio2.js
+++
@@ -1,9 +0,0 @@
-const rl = require('readline-sync')
-//exemplo vou comer até 100 coxinhas 
-let stomach = 0;
-
-// 0 < 100 = FALSE    101 < 100 = TRUE
-while(stomach < 100){
-    console.log('Quero comer mais coxinhas')
-    stomach = stomach + 10;
-}
--- Aula10-04/exercicio3.js
+++
@@ -1,7 +0,0 @@
-const rl = require('readline-sync')
-
-let number = 0;
-
-while((++number) < 11){
-    console.log(`${number}`)
-}
