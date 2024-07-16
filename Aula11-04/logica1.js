87026b91ec376f42378b7dce4315c84bc3cb80e2
gks <126225075+Gukisz@users.noreply.github.com>
2024-07-16 20:14:49 -0300

    Delete Aula11-04 directory

--- Aula11-04/logica1.js
+++
@@ -1,7 +0,0 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
-
-for(let number of numbers){
-    process.stdout.write(`${number} `)
-}
-process.stdout.write("\n")
-
--- Aula11-04/logica2.js
+++
@@ -1,5 +0,0 @@
-for (let i = 2; i <= 10; i += 2) {
-    process.stdout.write(`${i} `)
-}
-process.stdout.write("\n")
-
