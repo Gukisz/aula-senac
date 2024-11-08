fafaed7819926407ef57f9ea469f67dbdcaac949
gks <126225075+Gukisz@users.noreply.github.com>
2024-11-08 20:02:37 -0300

    Add files via upload

---
+++ Java/Aula01/Java/Atividade1.java
@@ -0,0 +1,28 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String meuNome = sc.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int minhaIdade = sc.nextInt();
+
+        System.out.print("Digite sua altura (em metros): ");
+        double minhaAltura = sc.nextDouble();
+
+        System.out.print("Você é aluno? (true/false): ");
+        boolean souAluno = sc.nextBoolean();
+
+        String mensagem = String.format(
+            "Meu nome é %s \nTenho %d anos \nMinha altura é: %.2f metros \nSou aluno: %b.", 
+            meuNome, minhaIdade, minhaAltura, souAluno
+        );
+
+        System.out.println(mensagem);
+    }
+}
